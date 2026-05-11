fa26a1a17ccfb293c61b244090c447d9ccf08963
VkTheEncoder <158612790+VkTheEncoder@users.noreply.github.com>
2026-05-11 16:13:54 +0530

    Add configuration variables to config.py
    Added configuration variables for bot and database connection.

---
+++ config.py
@@ -0,0 +1,7 @@
+BOT_TOKEN = "8512803170:AAFNIH89wWhkUiL9GP-CRpE2IaTYcuptaf4"
+FILE_BOT_USERNAME = "file_sharing_bot03_bot" # No @ symbol
+CHANNEL_USERNAME = "@Facky_Hindi_Donghua"
+WP_URL = "https://fackyhindidonghuas.in/wp-json/wp/v2"
+WP_USER = "TelegramBot"
+WP_APP_PASS = "XKX3 bKDs 9NUN IKim xgeR QIzS" # Your WP App Password
+MONGO_URI = "mongodb+srv://filesharing:cPnK4QJKan0XiFsa@cluster0.wbmz6ma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
