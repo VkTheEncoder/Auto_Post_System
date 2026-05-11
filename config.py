a910fc92713b6dffc9028a02bd75e9a99c6d684a
VkTheEncoder <158612790+VkTheEncoder@users.noreply.github.com>
2026-05-11 18:07:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ BOT_TOKEN = "8512803170:AAFNIH89wWhkUiL9GP-CRpE2IaTYcuptaf4"
 FILE_BOT_USERNAME = "file_sharing_bot03_bot" # No @ symbol
 CHANNEL_USERNAME = "@Facky_Hindi_Donghua"
 WP_URL = "https://fackyhindidonghuas.in/wp-json/wp/v2"
-WP_USER = "TelegramBot"
+WP_USER = "facky@admin0578"
 WP_APP_PASS = "XKX3 bKDs 9NUN IKim xgeR QIzS" # Your WP App Password
 MONGO_URI = "mongodb+srv://filesharing:cPnK4QJKan0XiFsa@cluster0.wbmz6ma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
