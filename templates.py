a532669f0721a9e253352f71f1d8d1171fa938ac
VkTheEncoder <158612790+VkTheEncoder@users.noreply.github.com>
2026-05-11 18:39:13 +0530

    Update templates.py

--- templates.py
+++ templates.py
@@ -1,30 +1,44 @@
-# D1 EXACT HTML (With Strict WordPress Line Breaks)
-D1_BLOCK = """<div class="wp-block-columns">
-<div class="wp-block-column is-vertically-aligned-center">
-<h3 class="wp-block-heading alignwide has-text-align-center">EPISODE {EPISODE_NUM}</h3>
-</div>
-<div class="wp-block-column">
-<div class="wp-block-buttons alignwide">
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="{LINK_1080}" rel="nofollow">1080p Download</a></div>
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">4K Download</a></div>
-</div>
-</div>
-</div>
-"""
+# D1 EXACT HTML (Original Spacing Preserved Safely)
+D1_BLOCK = (
+    '\n'
+    '<div class="wp-block-columns">\n'
+    '<div class="wp-block-column is-vertically-aligned-center">\n'
+    '<h3 class="wp-block-heading alignwide has-text-align-center">EPISODE {EPISODE_NUM}</h3>\n'
+    '</div>\n'
+    '\n'
+    '\n'
+    '\n'
+    '<div class="wp-block-column">\n'
+    '<div class="wp-block-buttons alignwide">\n'
+    '<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="{LINK_1080}" rel="nofollow">1080p Download</a></div>\n'
+    '\n'
+    '\n'
+    '\n'
+    '<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">4K Download</a></div>\n'
+    '</div>\n'
+    '</div>\n'
+    '</div>\n'
+    ''
+)
 
-
-# D2 EXACT HTML (With Strict WordPress Line Breaks)
-D2_BLOCK = """<div class="wp-block-columns">
-<div class="wp-block-column is-vertically-aligned-center">
-<h3 class="wp-block-heading alignwide has-text-align-center">EPISODE {EPISODE_NUM}</h3>
-</div>
-<div class="wp-block-column">
-<div class="wp-block-buttons alignwide">
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="{LINK_1080}" rel=" nofollow"> Download</a></div>
-</div>
-</div>
-</div>
-"""
+# D2 EXACT HTML (Original Spacing Preserved Safely)
+D2_BLOCK = (
+    '\n'
+    '<div class="wp-block-columns">\n'
+    '<div class="wp-block-column is-vertically-aligned-center">\n'
+    '<h3 class="wp-block-heading alignwide has-text-align-center">EPISODE {EPISODE_NUM}</h3>\n'
+    '</div>\n'
+    '\n'
+    '\n'
+    '\n'
+    '<div class="wp-block-column">\n'
+    '<div class="wp-block-buttons alignwide">\n'
+    '<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="{LINK_1080}" rel=" nofollow"> Download</a></div>\n'
+    '</div>\n'
+    '</div>\n'
+    '</div>\n'
+    ''
+)
 
 TELEGRAM_4K_MSG = """
 {DONGHUA_NAME} 4K (Hardsub) quality Added ✅
