1c6a7aa109cdf7fdfe6cd1f5b52ee23d6fd8bd28
VkTheEncoder <158612790+VkTheEncoder@users.noreply.github.com>
2026-05-11 19:19:20 +0530

    Update templates.py

--- templates.py
+++ templates.py
@@ -1,24 +1,45 @@
-D1_BLOCK = """<div class="wp-block-columns"><div class="wp-block-column is-vertically-aligned-center"><h3 class="wp-block-heading alignwide has-text-align-center">EPISODE {EPISODE_NUM}</h3>
-</div>
-<div class="wp-block-column"><div class="wp-block-buttons alignwide"><div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="{LINK_1080}">1080p Download</a></div>
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">4K Download</a></div>
-</div>
-</div>
-</div>
-"""
+D1_BLOCK = """<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:heading {"textAlign":"center","level":3,"align":"wide"} -->
+<h3 class="wp-block-heading alignwide has-text-align-center">EPISODE {EPISODE_NUM}</h3>
+<!-- /wp:heading --></div>
+<!-- /wp:column -->
 
-D2_BLOCK = """<div class="wp-block-columns"><div class="wp-block-column is-vertically-aligned-center"><h3 class="wp-block-heading alignwide has-text-align-center">EPISODE {EPISODE_NUM}</h3>
-</div>
-<div class="wp-block-column"><div class="wp-block-buttons alignwide"><div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="{LINK_1080}"> Download</a></div>
-</div>
-</div>
-</div>
-"""
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:buttons {"align":"wide","layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons alignwide"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="{LINK_1080}" rel="nofollow">1080p Download</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">4K Download</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->"""
+
+
+D2_BLOCK = """<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:heading {"textAlign":"center","level":3,"align":"wide"} -->
+<h3 class="wp-block-heading alignwide has-text-align-center">EPISODE {EPISODE_NUM}</h3>
+<!-- /wp:heading --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:buttons {"align":"wide","layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons alignwide"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="{LINK_1080}" rel="nofollow"> Download</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->"""
+
 
 TELEGRAM_4K_MSG = """
 {DONGHUA_NAME} 4K (Hardsub) quality Added ✅
 
-Size :- {FILE_SIZE} 💀 
+Size :- {FILE_SIZE} MB 💀 
 
 Now Enjoy Highest Quality 😌🔥
 """
