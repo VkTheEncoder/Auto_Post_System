7fe5b68085624734f7f2b6a0fa2ca3d211ade647
VkTheEncoder <158612790+VkTheEncoder@users.noreply.github.com>
2026-05-11 18:25:03 +0530

    Update templates.py

--- templates.py
+++ templates.py
@@ -1,17 +1,17 @@
-# Push these blocks all the way to the left margin!
-
+# D1 EXACT HTML
 D1_BLOCK = """<div class="wp-block-columns"><div class="wp-block-column is-vertically-aligned-center"><h3 class="wp-block-heading alignwide has-text-align-center">EPISODE {EPISODE_NUM}</h3>
 </div>
 <div class="wp-block-column"><div class="wp-block-buttons alignwide"><div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="{LINK_1080}" rel="nofollow">1080p Download</a></div>
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="{LINK_4K}" rel="nofollow">4K Download</a></div>
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">4K Download</a></div>
 </div>
 </div>
 </div>
 """
 
+# D2 EXACT HTML
 D2_BLOCK = """<div class="wp-block-columns"><div class="wp-block-column is-vertically-aligned-center"><h3 class="wp-block-heading alignwide has-text-align-center">EPISODE {EPISODE_NUM}</h3>
 </div>
-<div class="wp-block-column"><div class="wp-block-buttons alignwide"><div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="{LINK_1080}"> Download</a></div>
+<div class="wp-block-column"><div class="wp-block-buttons alignwide"><div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="{LINK_1080}" rel=" nofollow"> Download</a></div>
 </div>
 </div>
 </div>
