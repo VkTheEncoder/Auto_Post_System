58caf73274cb1c842989e71270a2fd810d7b5ba0
VkTheEncoder <158612790+VkTheEncoder@users.noreply.github.com>
2026-07-17 10:09:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,5 +10,5 @@ FILE_BOT_USERNAME = "file_sharing_bot03_bot" # No @ symbol
 CHANNEL_USERNAME = "@Facky_Hindi_Donghua"
 WP_URL = "https://fackyhindidonghuas.in/wp-json/wp/v2"
 WP_USER = "facky@admin0578"
-WP_APP_PASS = "65mF 2FOX HQAQ ZjfH GuoG hy2J" # Your WP App Password
+WP_APP_PASS = "eN6u 74WS 1fdX UTqy GlnF y0xp" # Your WP App Password
 MONGO_URI = "mongodb+srv://filesharing:cPnK4QJKan0XiFsa@cluster0.wbmz6ma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
